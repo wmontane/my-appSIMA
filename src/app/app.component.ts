f25d5a204807b8ce6bac26ae59a7fa642dc700f9
wmontane <wiler14@hotmail.com>
2022-01-11 16:50:44 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+
+/*
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+*/
+ template: '<h1>Hola SIMA  </h1>',
+ styles:
+ [
+  "color:blue;"
+ ]
+
+})
+export class AppComponent {
+  title = 'my-app';
+}
